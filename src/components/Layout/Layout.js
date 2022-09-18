63890157868f09cfc4050c8673acbcb7981f05ab
Charly Brown <2131406-carlosmonti@users.noreply.gitlab.com>
2022-09-18 00:40:52 -0300

    refactor: removing unneeded code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,16 +3,10 @@ import Header from '../Header';
 
 const Layout = () => {
   return (
-    <div>
+    <>
       <Header />
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
-    </div>
+    </>
   );
 };
 
